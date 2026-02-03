b88a8366f5a80fe844814e29e98b232bf4f1137d
Mahvia <mahviasajjadabbasi@gmail.com>
2026-02-03 18:47:26 +0500

    remove errors

--- apps/web/components/ui/JobDescription.tsx
+++ apps/web/components/ui/JobDescription.tsx
@@ -113,7 +113,7 @@ const JobDescription = ({ jobData }: any) => {
                 What we're looking for
               </h2>
               <div className="space-y-3">
-                {jobData.requirements.map((item, index) => (
+                {jobData.requirements.map((item:any, index:any) => (
                   <div key={index} className="flex items-start gap-3">
                     <div className="mt-1.5 shrink-0">
                       <div className="w-5 h-5 rounded-full  flex items-center justify-center">
